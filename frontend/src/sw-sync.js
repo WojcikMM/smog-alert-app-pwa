b8f404c3351f8471501e3c15fd9793d721467041
Wojcik Michal <m.mwojcik@outlook.com>
2020-12-28 20:01:16 +0100

    fix sw-sync.js

--- frontend/src/sw-sync.js
+++ frontend/src/sw-sync.js
@@ -1,39 +1,34 @@
-(async function () {
+class AirIndexSyncWorker {
 
-
-  class AirIndexSyncWorker {
-
-    init() {
-      this.listenPeriodicSync();
-      this.registerServiceWorker();
-    }
-
-    listenPeriodicSync() {
-      self.addEventListener('periodicsync', async event => {
-        console.log('periodic sync type:', event.type);
-      });
-    }
-
-    registerServiceWorker() {
-      if (navigator.serviceWorker) {
-        navigator.serviceWorker.getRegistrations()
-          .then(() => navigator.serviceWorker.ready)
-          .then(registration => {
-            if (registration.periodicSync) {
-              registration.periodicSync.register('content-sync', {
-                minInterval: 5000
-              });
-            } else {
-              console.log('Periodic sync not available');
-            }
-          })
-      }
-    }
+  init() {
+    this.listenPeriodicSync();
+    this.registerServiceWorker();
   }
 
-  window.addEventListener('load', () => {
-    const sw = new AirIndexSyncWorker();
-    sw.init();
-  });
+  listenPeriodicSync() {
+    self.addEventListener('periodicsync', async event => {
+      console.log('periodic sync type:', event.type);
+    });
+  }
 
-})();
+  registerServiceWorker() {
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.getRegistrations()
+        .then(() => navigator.serviceWorker.ready)
+        .then(registration => {
+          if (registration.periodicSync) {
+            registration.periodicSync.register('content-sync', {
+              minInterval: 5000
+            });
+          } else {
+            console.log('Periodic sync not available');
+          }
+        })
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  const sw = new AirIndexSyncWorker();
+  sw.init();
+});
